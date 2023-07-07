4e79aa4ebdbf343fd4d3f48e89ac0ea688e46792
Andrea Bertoli <andrea.bertpp@gmail.com>
2023-07-07 21:23:53 +0200

    wip

--- 03_arch_installer/01_pre_install.sh
+++ 03_arch_installer/01_pre_install.sh
@@ -39,6 +39,9 @@ iwctl device list
 iwctl station "$WIFI_DEVICE" scan
 iwctl station "$WIFI_DEVICE" get-networks
 iwctl --passphrase "$WIFI_PASSPHRASE" station "$WIFI_DEVICE" connect "$WIFI_SSID"
+echo "waiting for connection.."
+sleep 10
+
 ping -c 5 -w 10 8.8.8.8
 set +x
 
