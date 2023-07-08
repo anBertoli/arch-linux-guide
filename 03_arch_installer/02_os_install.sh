1eef578754f67bcbbb2b9b09869cca39e482fb3b
Andrea Bertoli <andrea.bertpp@gmail.com>
2023-07-08 18:54:19 +0200

    wip

--- 03_arch_installer/02_os_install.sh
+++ 03_arch_installer/02_os_install.sh
@@ -164,6 +164,7 @@ LOCALE_CONF="LANG=en_US.UTF-8"
 # uncomment chosen language then
 # generate and save locale files
 arch-chroot /mnt sed -i "/${LOCALE_GEN}/s/^#//gw /tmp/changes.txt" /etc/locale.gen
+exit 0
 check_file_not_empty_and_delete /mnt/tmp/changes.txt
 arch-chroot /mnt locale-gen
 arch-chroot /mnt touch /etc/locale.conf
