d521be1e5ce2644d2005333ba19dfcb688023fed
Andrea Bertoli <andrea.bertpp@gmail.com>
2023-07-09 16:47:59 +0200

    wip

--- 03_arch_installer/03_os_custom_chroot.sh
+++ 03_arch_installer/03_os_custom_chroot.sh
@@ -16,7 +16,8 @@ check_vars
 #######################################################################
 print_header_section "Getting user home"
 set -x
-USER_HOME="$(getent passwd "${USER_NAME}" | cut -d: -f6)"
+#USER_HOME="$(getent passwd "${USER_NAME}" | cut -d: -f6)"
+USER_HOME="${HOME}"
 if [ -z "$USER_HOME" ]
 then
       echo "USER_HOME is empty"
@@ -24,7 +25,7 @@ then
 fi
 set +x
 
-print_text "Root HOME is '${HOME}'."
+print_text "User HOME is '${HOME}'."
 print_text "User HOME is '${USER_HOME}'."
 prompt_continue "Continue?"
 
@@ -33,85 +34,65 @@ prompt_continue "Continue?"
 #######################################################################
 ######## USERSPACE PROGRAMS ###########################################
 #######################################################################
-#print_header_section "Programming languages and IDEs "
-#
-#set -x
-#cd ~
-#pacman --noconfirm -Syu
-#set +x
-#
-#### install go
-#print_checklist_item "install Go"
-#set -x
-#GO_VER=1.20
-#rm -rf /usr/local/go
-#
-#curl -L --output ./go${GO_VER}.linux-amd64.tar.gz https://go.dev/dl/go${GO_VER}.linux-amd64.tar.gz
-#tar -C /usr/local -xzf ./go${GO_VER}.linux-amd64.tar.gz
-#rm ./go${GO_VER}.linux-amd64.tar.gz
-#
-#set +x
-#print_text "Writing
-#'export PATH=${PATH}:/usr/local/go/bin' to '${USER_HOME}/.profile'"
-#set -x
-# TODO
-#echo "export PATH=${PATH}:/usr/local/go/bin" >> "$USER_HOME"/.profile
-#source "$HOME"/.profile
-#go version
-#set +x
-#
-#prompt_continue "Continue?"
-#
-#### install goland
-#print_checklist_item "install Goland"
-#set -x
-#GOLAND_VER="2022.2.4"
-#rm -rf /opt/GoLand*
-#
-#curl -L --output ./goland-${GOLAND_VER}.tar.gz https://download.jetbrains.com/go/goland-${GOLAND_VER}.tar.gz
-#tar xzf ./goland-${GOLAND_VER}.tar.gz -C /opt/
-#rm ./goland-${GOLAND_VER}.tar.gz
-#set +x
-#
-## TODO: CHECK
-#print_text "Writing
-#'export PATH=${PATH}:/opt/GoLand-${GOLAND_VER}/go/bin/goland.sh'
-#to '${HOME}/.profile' and '${USER_HOME}/.profile'"
-#set -x
-#echo "export PATH=${PATH}:/opt/GoLand-${GOLAND_VER}/go/bin/goland.sh" >> "$HOME"/.profile
-#echo "export PATH=${PATH}:/opt/GoLand-${GOLAND_VER}/go/bin/goland.sh" >> "$USER_HOME"/.profile
-#set +x
-#
-#prompt_continue "Continue?"
+print_header_section "Programming languages and IDEs "
 
-chown ${USER_NAME}:${USER_NAME} /home/${USER_NAME}/.profile
+set -x
+cd ~
+pacman --noconfirm -Syu
+set +x
+
+### install go
+print_checklist_item "install Go"
+set -x
+GO_VER=1.20
+rm -rf /usr/local/go
+
+curl -L --output ./go${GO_VER}.linux-amd64.tar.gz https://go.dev/dl/go${GO_VER}.linux-amd64.tar.gz
+tar -C /usr/local -xzf ./go${GO_VER}.linux-amd64.tar.gz
+rm ./go${GO_VER}.linux-amd64.tar.gz
+
+set +x
+print_text "Writing 'export PATH=${PATH}:/usr/local/go/bin' to '${USER_HOME}/.profile'"
+set -x
+echo "export PATH=${PATH}:/usr/local/go/bin" >> "$USER_HOME"/.profile
+source "$HOME"/.profile
+go version
+set +x
+
+prompt_continue "Continue?"
+
+### install goland
+print_checklist_item "install Goland"
+set -x
+GOLAND_VER="2022.2.4"
+rm -rf /opt/GoLand*
+
+curl -L --output ./goland-${GOLAND_VER}.tar.gz https://download.jetbrains.com/go/goland-${GOLAND_VER}.tar.gz
+tar xzf ./goland-${GOLAND_VER}.tar.gz -C /opt/
+rm ./goland-${GOLAND_VER}.tar.gz
+set +x
+
+print_text "Writing 'export PATH=${PATH}:/opt/GoLand-${GOLAND_VER}/go/bin/goland.sh' and '${USER_HOME}/.profile'"
+set -x
+echo "export PATH=${PATH}:/opt/GoLand-${GOLAND_VER}/go/bin/goland.sh" >> "$USER_HOME"/.profile
+set +x
+
+prompt_continue "Continue?"
+
+# TODO
+#chown "${USER_NAME}:${USER_NAME}" "/home/${USER_NAME}/.profile"
+
 
 
 ### install rustup, rust and cargo
 print_checklist_item "install Rust"
 set -x
 
-sudo -u "${USER_NAME}" /bin/bash -c "
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
-source ${USER_HOME}/.profile
+source "${USER_HOME}/.profile"
 rustup update
 cargo --version
-"
-
-source "${HOME}/.profile"
-cargo --version
 set +x
-
-#
-#print_text "Writing
-#'export PATH=${PATH}:~/.cargo/bin'
-#to '${HOME}/.profile' and '${USER_HOME}/.profile'"
-#set -x
-#echo "export PATH=${PATH}:~/.cargo/bin" >> "$HOME"/.profile
-#echo "export PATH=${PATH}:~/.cargo/bin" >> "$USER_HOME"/.profile
-#source "$HOME"/.profile
-#cargo --version
-#set +x
 
 prompt_continue "Continue?"
 
