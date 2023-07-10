48ca325c65d1b747943373aef58e1b4e1c36673b
Andrea Bertoli <andrea.bertpp@gmail.com>
2023-07-10 02:37:50 +0200

    wip

--- 03_arch_installer/03_os_custom_chroot.sh
+++ 03_arch_installer/03_os_custom_chroot.sh
@@ -27,10 +27,9 @@ cd "${HOME}"
 CLION_VER="2022.2.4"
 sudo rm -rf /opt/clion*
 
-pwd
-curl -L --output ./clion-${CLION_VER}.tar.gz https://download.jetbrains.com/cpp/CLion-${CLION_VER}.tar.gz
+#curl -L --output ./clion-${CLION_VER}.tar.gz https://download.jetbrains.com/cpp/CLion-${CLION_VER}.tar.gz
 sudo tar xzf ./clion-${CLION_VER}.tar.gz -C /opt/
-rm ./clion-${GOLAND_VER}.tar.gz
+rm ./clion-${CLION_VER}.tar.gz
 set +x
 
 print_text "Writing export PATH=${PATH}:/opt/clion-${CLION_VER}/bin/' to '${HOME}/.profile''"
